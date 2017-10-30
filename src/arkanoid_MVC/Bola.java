07bb6fe6713bfade74af1f1f19972cd1d15911ec
Esteban <egtissot@gmail.com>
2017-10-30 18:42:36 -0300

    Update Bola.java
    JENKINS - Prueba - duplicado de codigo y declaracion de variables que no se usan

--- src/arkanoid_MVC/Bola.java
+++ src/arkanoid_MVC/Bola.java
@@ -14,6 +14,8 @@ public class Bola extends Model implements Runnable, BeatModelInterface {
 	Thread thread;
 	Clip sonido;
 	int speed;
+	int a,b,c;
+	
 	ArrayList<BeatObserver> beatObservers = new ArrayList<BeatObserver>();
 	ArrayList<BPMObserver> bpmObservers = new ArrayList<BPMObserver>();
 
@@ -71,6 +73,7 @@ public class Bola extends Model implements Runnable, BeatModelInterface {
 	@Override
 	public void initialize() {
 		speed = 0;
+		speed = 0;
 	}
 
 	@Override
@@ -81,6 +84,7 @@ public class Bola extends Model implements Runnable, BeatModelInterface {
 	@Override
 	public void off() {
 		speed =0;
+		speed = 0;
 	}
 
 	@Override
